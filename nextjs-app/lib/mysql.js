ead15815d7ff6cc5b5b7a0bf2bc8f88841231a76
Yousef-olx <olxyo100@gmail.com>
2025-11-11 11:47:38 +0300

    Fix MySQL port configuration

--- nextjs-app/lib/mysql.js
+++ nextjs-app/lib/mysql.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-  port: 27906, // Replace with YOUR port from Aiven
+  port: process.env.MYSQL_PORT || 27906,
   ssl: {
     rejectUnauthorized: true
   },
